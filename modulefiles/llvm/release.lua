c9f4ea43d9c4d07e856f9da1d17cc857e1380289
Shilei Tian <i@tianshilei.me>
2024-07-22 10:25:00 -0400

    Add modulefiles

---
+++ modulefiles/llvm/release.lua
@@ -0,0 +1,18 @@
+-- -*- lua -*-
+
+whatis("Name    : llvm")
+whatis("Version : release")
+
+conflict("llvm", "lightning-llvm", "offload")
+
+local build_root = os.getenv("BUILD_ROOT")
+local pkg_home = pathJoin(build_root, myModuleName(), myModuleVersion())
+
+pushenv("LLVM_ROOT", pkg_home)
+pushenv("LLVM_PATH", pkg_home)
+
+prepend_path("PATH",              pathJoin(pkg_home, "bin"))
+prepend_path("CPATH",             pathJoin(pkg_home, "include"))
+prepend_path("LIBRARY_PATH",      pathJoin(pkg_home, "lib"))
+prepend_path("LD_LIBRARY_PATH",   pathJoin(pkg_home, "lib"))
+prepend_path("DYLD_LIBRARY_PATH", pathJoin(pkg_home, "lib"))
