56ad7e347fc0d877aa896354c6f5feb1d036a07b
Shilei Tian <i@tianshilei.me>
2024-08-06 22:42:59 -0400

    update module files for offload

--- modulefiles/offload/debug.lua
+++ modulefiles/offload/debug.lua
@@ -5,7 +5,7 @@ whatis("Version : debug")
 
 conflict("llvm", "lightning-llvm", "offload")
 
-local build_root = os.getenv("BUILD_ROOT")
+local build_root = os.getenv("DEPLOY_ROOT")
 local pkg_home = pathJoin(build_root, myModuleName(), myModuleVersion())
 
 pushenv("LLVM_ROOT", pkg_home)
@@ -14,5 +14,8 @@ pushenv("LLVM_PATH", pkg_home)
 prepend_path("PATH",              pathJoin(pkg_home, "bin"))
 prepend_path("CPATH",             pathJoin(pkg_home, "include"))
 prepend_path("LIBRARY_PATH",      pathJoin(pkg_home, "lib"))
+prepend_path("LIBRARY_PATH",      pathJoin(pkg_home, "lib", "x86_64-unknown-linux-gnu"))
 prepend_path("LD_LIBRARY_PATH",   pathJoin(pkg_home, "lib"))
+prepend_path("LD_LIBRARY_PATH",   pathJoin(pkg_home, "lib", "x86_64-unknown-linux-gnu"))
 prepend_path("DYLD_LIBRARY_PATH", pathJoin(pkg_home, "lib"))
+prepend_path("DYLD_LIBRARY_PATH", pathJoin(pkg_home, "lib", "x86_64-unknown-linux-gnu"))
