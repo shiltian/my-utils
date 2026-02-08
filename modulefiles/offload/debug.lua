aea46297c9dc04ca841c19ca27850a4a0fba03fe
Shilei Tian <i@tianshilei.me>
2026-02-08 16:26:38 -0500

    further improve modulefiles for llvm family

--- modulefiles/offload/debug.lua
+++ modulefiles/offload/debug.lua
@@ -1,32 +1,8 @@
 -- -*- lua -*-
+local base = myFileName():match("(.*)/[^/]+/[^/]+%.lua$")
+local pkg_home = loadfile(pathJoin(base, ".common", "llvm_base.lua"))()
 
-whatis("Name    : offload")
-whatis("Version : debug")
-
--- Define all conflicting modules
-local conflicts = {"llvm", "lightning-llvm", "offload"}
-
--- Automatically unload conflicting modules if they are loaded
-for _, mod in ipairs(conflicts) do
-    if isloaded(mod) then
-        unload(mod)
-    end
-end
-
--- Declare conflicts
-conflict(table.unpack(conflicts))
-
-local build_root = os.getenv("BUILD_ROOT")
-local pkg_home = pathJoin(build_root, myModuleName(), myModuleVersion())
-
-pushenv("LLVM_ROOT", pkg_home)
-pushenv("LLVM_PATH", pkg_home)
-
-prepend_path("PATH",              pathJoin(pkg_home, "bin"))
-prepend_path("CPATH",             pathJoin(pkg_home, "include"))
-prepend_path("LIBRARY_PATH",      pathJoin(pkg_home, "lib"))
+-- offload-specific: additional arch-qualified lib paths
 prepend_path("LIBRARY_PATH",      pathJoin(pkg_home, "lib", "x86_64-unknown-linux-gnu"))
-prepend_path("LD_LIBRARY_PATH",   pathJoin(pkg_home, "lib"))
 prepend_path("LD_LIBRARY_PATH",   pathJoin(pkg_home, "lib", "x86_64-unknown-linux-gnu"))
-prepend_path("DYLD_LIBRARY_PATH", pathJoin(pkg_home, "lib"))
 prepend_path("DYLD_LIBRARY_PATH", pathJoin(pkg_home, "lib", "x86_64-unknown-linux-gnu"))
